0ab8124296cd49a41ee2c67210206a4ce5a0475d
Zen Ahmed <zaa040690@gmail.com>
2024-10-09 11:49:48 -0400

    fixed errors

--- app/components/MainTestimonial.tsx
+++ app/components/MainTestimonial.tsx
@@ -22,7 +22,7 @@ const MainTestimonial: React.FC<MainTestimonialProps> = ({
             <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
           </svg>
           <blockquote className="text-2xl font-semibold text-gray-900 mb-8">
-            "{quote}"
+            {`"${quote}"`}
           </blockquote>
           <div className="flex items-center justify-center">
             <Image
