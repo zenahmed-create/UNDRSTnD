4fbb65b641dcde04729c066506fa13e05fd5a1ac
Zen Ahmed <zaa040690@gmail.com>
2024-10-09 16:02:18 -0400

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "UNDRSTnD",
   description: "Get Smarter, Faster",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
